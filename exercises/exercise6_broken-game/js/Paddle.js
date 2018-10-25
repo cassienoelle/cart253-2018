25df6ee4952ddd8d12230cda0a43067a0abf3bc3
cassienoelle <cassienoelle@gmail.com>
2018-10-25 16:33:39 -0400

    Ex6 - fixed error 24
    Paddle.js
    line 29: changed keyDown to keyIsDown

--- exercises/exercise6_broken-game/js/Paddle.js
+++ exercises/exercise6_broken-game/js/Paddle.js
@@ -26,7 +26,7 @@ Paddle.prototype.handleInput = function() {
   if (keyIsDown(this.upKey)) {
     this.vy = -this.speed;
   }
-  else if (keyDown(downKey)) {
+  else if (keyIsDown(downKey)) {
     this.vy = -this.speed;
   }
 }
